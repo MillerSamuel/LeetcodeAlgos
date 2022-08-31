4a33fd0836cabb383a56323876704990532ff441
Samuel Miller <smillercb@gmail.com>
2022-08-31 16:38:04 -0400

    58

---
+++ 58.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let len=0;
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i]!=" "){
+            len+=1;
+        }
+        else if(len>0){
+            return len;
+        }
+    }
+    return len;
+};
